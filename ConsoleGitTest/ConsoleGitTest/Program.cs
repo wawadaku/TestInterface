952e899a50f4330827b58d5acec1b7c0e8b791da
大锅 <704751848@qq.com>
2019-09-06 11:10:00 +0800

    九九乘法

--- ConsoleGitTest/ConsoleGitTest/Program.cs
+++ ConsoleGitTest/ConsoleGitTest/Program.cs
@@ -7,6 +7,15 @@ namespace ConsoleGitTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}*{1}={2}\t",i,j,i*j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
